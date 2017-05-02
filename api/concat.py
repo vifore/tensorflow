3d79d7b3cbc97258ae4ceccec63f39ecca8082b7
liang <liang>
2017-05-02 16:44:21 +0900

    concat

---
+++ api/concat.py
@@ -0,0 +1,12 @@
+import tensorflow as tf
+
+tf.InteractiveSession()
+
+x = [[1,2,3], [4,5,6]]
+y = [[7,8,9], [10,11,12]]
+
+z = tf.concat([x,y], 0)
+print(z.eval())
+
+z = tf.concat([x,y], 1)
+print(z.eval())
